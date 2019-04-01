86b3bffce96de82d5a80bb28a68a84f38c70d5a4
Mauricio <maugravena@gmail.com>
2019-04-01 20:17:16 -0300

    create LogoBotcamp and reset.css

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import BtnBotcamp from './components/BtnBotcamp'
+import LogoBotcamp from './components/LogoBotcamp'
 
 class App extends Component {
   render() {
-    return <BtnBotcamp />
+    return (
+      <>
+        <LogoBotcamp />
+        <BtnBotcamp />
+      </>
+    )  
   }
 }
 
---
+++ app/src/components/LogoBotcamp/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './style.css'
+import logo from './botcamp.png'
+
+const LogoBotcamp = () => (
+  <img className="logo-botcamp" src={logo} alt="Logo Botcamp"/>
+)
+
+export default LogoBotcamp
