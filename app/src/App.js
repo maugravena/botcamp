e1213bdca84bb060791d2c0650e0397c47717b7d
Mauricio <maugravena@gmail.com>
2019-04-02 17:51:04 -0300

    create LoginWrapper component and setup the styles components

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 
+import LoginWrapper from './components/LoginWrapper';
 import BtnBotcamp from './components/BtnBotcamp'
 import LogoBotcamp from './components/LogoBotcamp'
 
@@ -8,10 +9,10 @@ import './styles/reset.css'
 class App extends Component {
   render() {
     return (
-      <>
+      <LoginWrapper>
         <LogoBotcamp />
         <BtnBotcamp />
-      </>
+      </LoginWrapper>
     )  
   }
 }
