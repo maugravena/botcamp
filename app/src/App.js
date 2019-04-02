f38695ada6cac994ae6efc46cc6a9cd08faee52b
Mauricio <maugravena@gmail.com>
2019-04-02 16:15:37 -0300

    chanded the place of reset.css

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
+
 import BtnBotcamp from './components/BtnBotcamp'
 import LogoBotcamp from './components/LogoBotcamp'
+
+import './styles/reset.css'
 
 class App extends Component {
   render() {
