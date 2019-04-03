447249f54092dd3742e90588aaa39ac5fd8f8acd
Mauricio <maugravena@gmail.com>
2019-04-03 17:05:25 -0300

    create pages structure

--- app/src/components/LoginWrapper/index.js
+++ app/src/components/LoginWrapper/index.js
@@ -4,13 +4,13 @@ import background from '../../img/ecossistema-background.png'
 
 const LoginWrapper = styled.main`
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
   background-image: url(${background});
   width: 100vw;
   height: 100vh;
   padding: 120px 0 80px 0;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
 `
 
 export default LoginWrapper
--- app/src/components/LogoBotcamp/index.js
+++ app/src/components/LogoBotcamp/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 import logo from './../../img/botcamp.png'
 
-const LogoBotcamp = styled.img.attrs({src: `${logo}`, alt: 'Logo Botcamp'})`
+const LogoBotcamp = styled.img.attrs({src: logo, alt: 'Logo Botcamp'})`
   max-width: 100%;
   height: auto;
   object-fit: contain;
