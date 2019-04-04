5efe1d34057738358a36e373b4301e906f48a5d6
Mauricio <maugravena@gmail.com>
2019-04-04 14:33:23 -0300

    delete align-items from header component

--- app/src/components/HeaderBotcamp/index.js
+++ app/src/components/HeaderBotcamp/index.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 const HeaderBotcamp = styled.header`
   display: flex;
   justify-content: space-between;
-  align-items: flex-start;
   padding: 10px 16px;
   background-color: #26265e;
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.25);
