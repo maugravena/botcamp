63974a44006b7dd712cdaf8d12db3d41db254200
Mauricio <maugravena@gmail.com>
2019-04-04 13:37:43 -0300

    changed position values

--- app/src/components/HeaderBotcamp/index.js
+++ app/src/components/HeaderBotcamp/index.js
@@ -4,8 +4,7 @@ const HeaderBotcamp = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
-  padding: 10px 11px 0 16px;
-  height: 66px;
+  padding: 10px 16px;
   background-color: #26265e;
   box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.25);
 `
