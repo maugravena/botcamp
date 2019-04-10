eb484b59e31f6f7b28f3892343a9290826e7bdf1
Mauricio <maugravena@gmail.com>
2019-04-10 10:55:50 -0300

    adicionando cor na finte do input

--- app/src/components/InputMessage/index.js
+++ app/src/components/InputMessage/index.js
@@ -5,6 +5,7 @@ const InputMessage = styled.input.attrs({type: 'text', placeholder: 'Diz aí...'
   height: 50px;
   border: none;
   font-size: 20px;
+  font-color: rgba(0, 0, 0, .33)
   background-color: #fbfbff;
   box-shadow: 0 -2px 8px 0 rgba(0, 0, 0, 0.1);
 `
